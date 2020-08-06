2f0da438043be882bec1e59853e6c7707b2bfc97
Roderic Campbell <roderic@gmail.com>
2020-08-05 21:42:07 -0700

    Remove unused alert view logic

--- GardenARClient/Views/AddItemsButtons.swift
+++ GardenARClient/Views/AddItemsButtons.swift
@@ -9,62 +9,6 @@
 import SwiftUI
 import Combine
 
-extension UIView {
-
-    // Since these are all static (and a hack) we can have it on whatever type we want, maybe shouldn't be on View
-    static func creationAlert(title: String, placeholder: String, completion: @escaping (String) -> ()) {
-        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
-        alert.addTextField() { textField in
-            textField.placeholder = placeholder
-        }
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { _ in })
-        alert.addAction(UIAlertAction(title: "Ok", style: .default) { _ in
-            guard let textField = alert.textFields?.first,
-                let inputtedText = textField.text else {
-                    return
-            }
-            completion(inputtedText)
-        })
-        showAlert(alert: alert)
-    }
-
-    static func showAlert(alert: UIAlertController) {
-        if let controller = topMostViewController() {
-            controller.present(alert, animated: true)
-        }
-    }
-
-    static private func topMostViewController() -> UIViewController? {
-        guard let rootController = keyWindow()?.rootViewController else {
-            return nil
-        }
-        return topMostViewController(for: rootController)
-    }
-
-    static private func topMostViewController(for controller: UIViewController) -> UIViewController {
-        if let presentedController = controller.presentedViewController {
-            return topMostViewController(for: presentedController)
-        } else if let navigationController = controller as? UINavigationController {
-            guard let topController = navigationController.topViewController else {
-                return navigationController
-            }
-            return topMostViewController(for: topController)
-        } else if let tabController = controller as? UITabBarController {
-            guard let topController = tabController.selectedViewController else {
-                return tabController
-            }
-            return topMostViewController(for: topController)
-        }
-        return controller
-    }
-    static private func keyWindow() -> UIWindow? {
-        return UIApplication.shared.connectedScenes
-            .filter {$0.activationState == .foregroundActive}
-            .compactMap {$0 as? UIWindowScene}
-            .first?.windows.filter {$0.isKeyWindow}.first
-    }
-}
-
 struct AddItemsButtons: View {
     @EnvironmentObject var store: Store<ViewModel>
     @Environment(\.presentationMode) var presentationMode
@@ -88,7 +32,6 @@ struct AddItemsButtons: View {
 
                 }
             }
-
         }.padding()
     }
 }
